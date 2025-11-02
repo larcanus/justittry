559ef0f9be95c139f753c80127a80542832b98cd
larcanus <larcanus@yandex.ru>
2025-11-02 16:57:05 +0300

    reorganization footer 1

--- src/modules/footer/components/footdown.jsx
+++ src/modules/footer/components/footdown.jsx
@@ -5,12 +5,12 @@ const Footdown = () => {
     const versionApp = dataPackage.version;
     const showMail = async (e) => {
         e.target.innerHTML.length < 30 ?
-            e.target.innerHTML += ' | larcanus@yandex.ru' : e.target.innerHTML = `Author of Rylkov A. | v${versionApp}`;
+            e.target.innerHTML += ' | alex.rulser@gmail.com' : e.target.innerHTML = `Author of AlexRulSer | v${versionApp}`;
     }
 
     return (
         <div className='divFoot'>
-            <p className='p-author'>copyright © 2021 | <a onClick={showMail}>Author of Rylkov A. | v{versionApp} </a>
+            <p className='p-author'>copyright © 2021 | <a onClick={showMail}>Author of AlexRulSer | v{versionApp} </a>
             </p>
         </div>
     );
