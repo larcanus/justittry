3f58ef5d76ba2b6c1635e945aba0842f4d863ea3
larcanus <larcanus@yandex.ru>
2025-11-02 17:10:48 +0300

    reorganization footer 1

--- src/modules/footer/components/footdown.jsx
+++ src/modules/footer/components/footdown.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import dataPackage from '../../../../package.json';
+import './../styles/style.css';
 
 const Footdown = () => {
     const versionApp = dataPackage.version;
-    const showMail = async (e) => {
-        e.target.innerHTML.length < 30 ?
-            e.target.innerHTML += ' | alex.rulser@gmail.com' : e.target.innerHTML = `Author of AlexRulSer | v${versionApp}`;
-    }
+    const [showEmail, setShowEmail] = useState(false);
+
+    const toggleEmail = () => {
+        setShowEmail(!showEmail);
+    };
+
+    const getDisplayText = () => {
+        const baseText = `© 2024 | Author: AlexRulSer | v${versionApp}`;
+
+        if (showEmail) {
+            return `${baseText} | alex.rulser@gmail.com`;
+        }
+
+        return baseText;
+    };
 
     return (
-        <div className='divFoot'>
-            <p className='p-author'>copyright © 2021 | <a onClick={showMail}>Author of AlexRulSer | v{versionApp} </a>
-            </p>
-        </div>
+        <footer className="footer">
+            <div className="footer-content">
+                <p className="author-info">
+                    {getDisplayText()}
+                    <button
+                        className="email-toggle"
+                        onClick={toggleEmail}
+                        aria-label={showEmail ? "Hide email" : "Show email"}
+                    >
+                        {showEmail ? ' [Hide]' : ' [Contact]'}
+                    </button>
+                </p>
+            </div>
+        </footer>
     );
 };
 
