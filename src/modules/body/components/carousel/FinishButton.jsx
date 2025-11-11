7c3e8ab4d615b89248d71ed44ba93378f14ac4bb
larcanus <larcanus@yandex.ru>
2025-11-11 20:48:09 +0300

    ref styles final button

--- src/modules/body/components/carousel/FinishButton.jsx
+++ src/modules/body/components/carousel/FinishButton.jsx
@@ -2,22 +2,33 @@ import React from 'react';
 
 /**
  * Кнопка завершения теста
+ *
+ * @param {Object} props - Свойства компонента
+ * @param {Function} props.onClick - Обработчик клика
+ * @param {boolean} props.showingAnswers - Флаг отображения ответов
  */
 const FinishButton = ({ onClick, showingAnswers }) => {
     const buttonText = showingAnswers
         ? '📊 Вернуться к результатам'
         : 'Закончить тест!';
 
-    const modifierClass = showingAnswers ? 'finish-button--results' : '';
+    const modifierClass = showingAnswers ? 'finish-button__main--results' : '';
 
     return (
-        <button
-            id='btnFinal'
-            className={`finish-button ${modifierClass}`.trim()}
-            onClick={onClick}
-        >
-            {buttonText}
-        </button>
+        <div className="finish-button">
+            <div className="finish-button__container">
+                <button
+                    id="btnFinal"
+                    className={`finish-button__main ${modifierClass}`.trim()}
+                    onClick={onClick}
+                    type="button"
+                >
+                    <span className="finish-button__text">
+                        {buttonText}
+                    </span>
+                </button>
+            </div>
+        </div>
     );
 };
 
